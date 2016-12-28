93f180303e7312fe5cb32a20f785dbdadfeb7d55
YSergey Ivanov <inkwell.nv@gmail.com>
2016-12-28 22:07:53 +0500

    finished students crud, fixed

--- src/main/java/com/smarterama/university/servlets/StudentServlet.java
+++ src/main/java/com/smarterama/university/servlets/StudentServlet.java
@@ -17,17 +17,19 @@ public class StudentServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getParameter("action");
-        int id = Integer.parseInt(request.getParameter("id"));
-        serviceStudent.setId(id);
+        String action = request.getParameter("action") != null ? request.getParameter("action") : "";
         try {
             switch (action) {
                 case "delete": {
+                    int id = Integer.parseInt(request.getParameter("id"));
+                    serviceStudent.setId(id);
                     serviceStudent.delete();
                     response.sendRedirect("/university/students");
                     break;
                 }
                 case "update": {
+                    int id = Integer.parseInt(request.getParameter("id"));
+                    serviceStudent.setId(id);
                     serviceStudent.retrieve();
                     request.getSession().setAttribute("id", id);
                     request.getSession().setAttribute("updatedStudent", serviceStudent);
@@ -75,21 +77,6 @@ public class StudentServlet extends HttpServlet {
             }
             response.sendRedirect("/university/students");
         } catch (PersistenceException | NumberFormatException e) {
-            String error = "Error: " + e.getMessage();
-            request.getSession().setAttribute("error", error);
-            getServletContext().getRequestDispatcher("/WEB-INF/views/create/student.jsp").forward(request, response);
-        }
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int deletingId = Integer.parseInt(request.getParameter("id"));
-        Student deletedStudent = new Student();
-        deletedStudent.setId(deletingId);
-        try {
-            deletedStudent.delete();
-            response.sendRedirect("/university/students");
-        } catch (PersistenceException e) {
             String error = "Error: " + e.getMessage();
             request.getSession().setAttribute("error", error);
             getServletContext().getRequestDispatcher("/WEB-INF/views/create/student.jsp").forward(request, response);
